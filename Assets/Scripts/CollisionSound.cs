6a4c805040a7acf6c952f0d6127d060ce82a6867
prashant <prashant@voodle.me>
2021-08-14 20:18:20 -0700

    adding unity project

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CollisionSound : MonoBehaviour
+{
+    public AudioClip Sound;
+
+    private AudioSource m_AudioSource;
+
+    //Play the music
+    private bool m_Played = false;
+
+    private void Start()
+    {
+        //Fetch the AudioSource from the GameObject
+        m_AudioSource = GetComponent<AudioSource>();
+        m_AudioSource.clip = Sound;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.name != "BgRain")
+        {
+            m_AudioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour
+{
+    public Camera MainCamera;
+
+    private void OnMouseDrag()
+    {
+        Vector3 worldPoint = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = new Vector3(worldPoint.x, worldPoint.y, 0);
+    }
+}
